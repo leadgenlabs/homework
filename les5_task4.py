a8132bc65240a74140cfa927ede17efe230c4284
leadgenlabs.ru <work@leadgenlabs.ru>
2020-06-28 17:12:43 +0300

    add files of lesson 5

---
+++ les5_task4.py
@@ -0,0 +1,30 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        less5 Task4
+# v: 3.8.2
+# Created:     26.06.2020
+#-------------------------------------------------------------------------------
+''' Создать (не программно) текстовый файл со следующим содержимым:
+One — 1
+Two — 2
+Three — 3
+Four — 4
+Необходимо написать программу, открывающую файл на чтение и считывающую
+построчно данные. При этом английские числительные должны заменяться на русские.
+Новый блок строк должен записываться в новый текстовый файл. '''
+
+def transl(sourse: str) -> str:
+    """ returns
+    :param sourse: sourse string
+    :return: translated string
+    """
+    tr_dic = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+    temp = sourse.split(' — ')
+    return '{0} — {1}'.format(tr_dic[temp[0]],temp[1])
+
+with open ('task4.txt', 'r') as file:
+    strings = file.readlines()
+
+out = map(transl, strings)
+
+with open ('task4_1.txt', 'w') as file:
+    file.write(''.join(out))
---
+++ les5_task5.py
@@ -0,0 +1,20 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        less5 Task5
+# v: 3.8.2
+# Created:     26.06.2020
+#-------------------------------------------------------------------------------
+''' Создать (программно) текстовый файл, записать в него программно набор чисел,
+разделенных пробелами. Программа должна подсчитывать сумму чисел в файле
+выводить ее на экран. '''
+
+import random
+
+num = 20 #number of elements
+with open ('task5.txt', 'w') as file:
+    string = [str(random.randint(0, 200)) for idn in range(num)]
+    file.write(' '.join(string))
+
+with open ('task5.txt', 'r') as file:
+    strings = file.read()
+    print(sum(map(int, strings.split(' '))))
+
